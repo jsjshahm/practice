4c9af1952eff74c13b300ee7b6a88006833c926a
jsjshahm <107354507+jsjshahm@users.noreply.github.com>
2022-11-01 20:04:01 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -0,0 +1,10 @@
+function solution(sides) {
+    if(
+    sides[0] + sides[1] > sides[2] && 
+        sides[1] + sides[2] > sides[0] &&
+        sides[2] + sides[0] > sides[1] ) {
+        return 1
+} else {
+    return 2
+}
+}
