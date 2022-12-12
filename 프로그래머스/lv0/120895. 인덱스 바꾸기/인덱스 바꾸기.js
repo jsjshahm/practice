b7dcbb2815e87dd577d3e644c9db8462409711b9
jsjshahm <107354507+jsjshahm@users.noreply.github.com>
2022-12-12 22:00:09 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,8 +1,6 @@
 function solution(my_string, num1, num2) {
     const array = [...my_string]
-    if(num1 < num2){
     const answer = array.splice(num2, 1, array[num1])
     array.splice(num1, 1, answer)
-    }
     return array.join('')
 }
