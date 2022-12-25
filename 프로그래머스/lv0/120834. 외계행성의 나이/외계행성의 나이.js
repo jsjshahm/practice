f73285c515f37a5ea3dc01567a11136d73b8ca09
jsjshahm <107354507+jsjshahm@users.noreply.github.com>
2022-12-25 17:28:56 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,16 +1,3 @@
 function solution(age) {
-    let basic = {
-        "0": "a",
-        "1": "b",
-        "2": "c",
-        "3": "d",
-        "4": "e",
-        "5": "f",
-        "6": "g",
-        "7": "h",
-        "8": "i",
-        "9": "j",
-    }
-    let answer = [...String(age)].map((element1) => basic[element1]).join("")
-    return answer
+    return age.toString().split("").map((v) => 'abcdefghij'[v]).join("")
 }
