62759f64e8ec1cd0df11d4d3bb00502551f71134
jsjshahm <107354507+jsjshahm@users.noreply.github.com>
2023-01-25 09:40:14 +0900

    [Bronze V] Title: A/B, Time: 220 ms, Memory: 17932 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -2,14 +2,13 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String args[]){
-        double A, B, division;
+        double A, B;
         Scanner scan = new Scanner(System.in);
         
         A = scan.nextDouble();
         B = scan.nextDouble();
-        division = A / B;
         
-        System.out.println(division);
+        System.out.println(A / B);
         scan.close();
     }
 }
